6880b359ce220f5dd9aa3cf07e448e0303f61547
aespinro <aespin@gcelsa.com>
2020-12-31 19:39:38 +0100

    working

--- proto.sh
+++ proto.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 echo "*** PROTOBUFFER GENERATING ***"
 protoc --proto_path=$GOPATH/src:. --gofast_out=plugins=grpc:. ./pb/*.proto
+protoc --proto_path=$GOPATH/src:. --nanopb_out=. ./pb/domo.proto
 echo "*** DONE PROTOBUFFER GENERATING ***"
