6a56b0891e593643b1037308dd4b76f4a8208085
aespinro <aespin@gcelsa.com>
2021-01-09 20:48:39 +0100

    working

--- cmd/service/service.go
+++ cmd/service/service.go
@@ -7,16 +7,19 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/ea3hsp/iot-api/configs"
 	"github.com/ea3hsp/iot-api/internal/api"
-	httptransport "github.com/ea3hsp/iot-api/internal/api/http"
 	"github.com/ea3hsp/iot-api/internal/backend/mqtt"
 
+	httptransport "github.com/ea3hsp/iot-api/internal/api/http"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	stdprometheus "github.com/prometheus/client_golang/prometheus"
+
+	"github.com/cloudflare/tableflip"
 )
 
 // RunService execute main function
@@ -34,13 +37,32 @@ func RunService() {
 			"caller", log.DefaultCaller,
 		)
 	}
-	//
-	errs := make(chan error)
+	pidFile := fmt.Sprintf("%s.pid", cfg.ProcessName)
+	upg, err := tableflip.New(tableflip.Options{
+		PIDFile: pidFile,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer upg.Stop()
+	// Do an upgrade on SIGHUP
 	go func() {
-		c := make(chan os.Signal)
-		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-		errs <- fmt.Errorf("%s", <-c)
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGHUP)
+		for range sig {
+			err := upg.Upgrade()
+			if err != nil {
+				level.Error(logger).Log("msg", fmt.Sprintf("Upgrade failed: %s", err.Error()))
+			}
+		}
 	}()
+	// //
+	// errs := make(chan error)
+	// go func() {
+	// 	c := make(chan os.Signal)
+	// 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	// 	errs <- fmt.Errorf("%s", <-c)
+	// }()
 	// create mqtt config
 	mqttCfg := mqtt.Config{
 		Brokers: []string{
@@ -82,15 +104,54 @@ func RunService() {
 	endpoints := api.MakeEndpoints(srv)
 	// services context
 	ctx := context.Background()
+
+	// // creates REST API Server
+	// go func() {
+	// 	// banner
+	// 	level.Info(logger).Log("msg", fmt.Sprintf("domo api worker API listening: %s", cfg.HTTPBindAddr))
+	// 	// service http handler
+	// 	hdl := httptransport.NewHTTPServer(ctx, endpoints)
+	// 	// start http server
+	// 	http.ListenAndServe(cfg.HTTPBindAddr, hdl)
+	// }()
+
 	// creates REST API Server
+	// Listen must be called before Ready
+	ln, err := upg.Listen("tcp", cfg.HTTPBindAddr)
+	if err != nil {
+		level.Error(logger).Log("msg", fmt.Sprintf("Can't listen: %s", err.Error()))
+		os.Exit(1)
+	}
+	// service http handler
+	hdl := httptransport.NewHTTPServer(ctx, endpoints)
+	// set server params
+	server := http.Server{
+		Handler:           hdl,
+		ReadTimeout:       0,
+		ReadHeaderTimeout: 0,
+		WriteTimeout:      0,
+		IdleTimeout:       0,
+	}
+	//
 	go func() {
-		// banner
-		level.Info(logger).Log("msg", fmt.Sprintf("domo api worker API listening: %s", cfg.HTTPBindAddr))
-		// service http handler
-		hdl := httptransport.NewHTTPServer(ctx, endpoints)
 		// start http server
-		http.ListenAndServe(cfg.HTTPBindAddr, hdl)
+		server.Serve(ln)
+		if err != http.ErrServerClosed {
+			level.Error(logger).Log("msg", fmt.Sprintf("Can't listen: %s", err.Error()))
+		}
 	}()
-	level.Error(logger).Log("msg", fmt.Sprintf("exit %v", <-errs))
+	//
+	if err := upg.Ready(); err != nil {
+		panic(err)
+	}
+	<-upg.Exit()
+	//
+	time.AfterFunc(30*time.Second, func() {
+		level.Info(logger).Log("msg", "Graceful shutdown timed out")
+		os.Exit(1)
+	})
+	// Wait for connections to drain.
+	server.Shutdown(context.Background())
+	//level.Error(logger).Log("msg", fmt.Sprintf("exit %v", <-errs))
 	backend.Disconnect()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ea3hsp/iot-api
 go 1.15
 
 require (
+	github.com/cloudflare/tableflip v1.2.0
 	github.com/eclipse/paho.mqtt.golang v1.3.1
 	github.com/go-chi/chi v1.5.1
 	github.com/go-chi/cors v1.1.1
